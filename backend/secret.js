f57f89c40bb641ae486657592f964f2b27beb737
Wade Willequer <Wade.Willequer@gmail.com>
2019-11-25 10:11:27 -0600

    Added in gitignore to ignore node modules installed packages for server

--- backend/secret.js
+++
@@ -1,8 +0,0 @@
-const secrets = {
-    //the url that we use to connect to the mongodb atlas cluster
-    dbUrl: 'mongodb+srv://WadeW:skrhqla420@cluster0-5xxfs.mongodb.net/test?retryWrites=true&w=majority'
-};
-
-const getSecret = key => secrets[key];
-
-module.exports = getSecret;
